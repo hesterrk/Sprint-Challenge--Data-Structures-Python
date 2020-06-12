7a7639f089734c79475352dbdaf4a93038db64b5
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-12 08:20:30 +0100

    set up of ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,11 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # To make our fixed size array: we have to pad it out with 'None' as each item within its capacity (5)
+        # [None, None, None, None] and then as we append elements at each index within the capacity it replaces the None value with correct value
+        self.storage = [None] * capacity
+        # The index of items as we go through the list
+        self.curr_index = 0
 
     def append(self, item):
         pass
