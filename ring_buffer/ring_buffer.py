44b28d55af54a00664b075ce34eb37fd9eee0432
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-12 08:27:03 +0100

    tests pass for ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,14 +1,55 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        # To make our fixed size array: we have to pad it out with 'None' as each item within its capacity (5)
-        # [None, None, None, None] and then as we append elements at each index within the capacity it replaces the None value with correct value
+        # To make a fixed size array: we have to pad it out with 'None' as each item within its capacity (5)
         self.storage = [None] * capacity
         # The index of items as we go through the list
         self.curr_index = 0
 
     def append(self, item):
-        pass
+
+        # print(self.storage)
+        # first item 'a' it will get assigned the  index (0) --> as thats the default index set in our bufferclass
+        self.storage[self.curr_index] = item
+        # print(self.storage[0], 'first item')
+
+        if (self.curr_index + 1) == self.capacity:
+            # We make the current item's (newest item) index to the oldest item
+            self.curr_index = 0
+
+        else:
+            # If capacity not reached, condition is hit until capacity is reached
+            # moves index along
+            self.curr_index += 1
+
+        return item
 
     def get(self):
-        pass
+        all_items = []
+
+        for item in self.storage:
+            if item is not None:
+                all_items.append(item)
+
+        return all_items
+
+
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']
